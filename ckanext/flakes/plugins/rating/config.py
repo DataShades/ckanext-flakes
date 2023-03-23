6db114a668d6419dfa8084b321fe5b107678d8e7
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-03-23 20:27:01 +0200

    feat: default rating widget for flakes_rating

--- ckanext/flakes/plugins/rating/config.py
+++ ckanext/flakes/plugins/rating/config.py
@@ -8,6 +8,9 @@ DEFAULT_MAX_RATING = 5
 CONFIG_MIN_RATING = "ckanext.flakes_rating.min_rating"
 DEFAULT_MIN_RATING = 1
 
+CONFIG_SHOW_WIDGET = "ckanext.flakes_rating.show_package_widget"
+DEFAULT_SHOW_WIDGET = False
+
 
 def max_rating() -> int:
     return tk.asint(tk.config.get(CONFIG_MAX_RATING, DEFAULT_MAX_RATING))
@@ -15,3 +18,7 @@ def max_rating() -> int:
 
 def min_rating() -> int:
     return tk.asint(tk.config.get(CONFIG_MIN_RATING, DEFAULT_MIN_RATING))
+
+
+def show_package_widget() -> bool:
+    return tk.asbool(tk.config.get(CONFIG_SHOW_WIDGET, DEFAULT_SHOW_WIDGET))
--- ckanext/flakes/plugins/rating/helpers.py
+++ ckanext/flakes/plugins/rating/helpers.py
@@ -4,6 +4,8 @@ import ckan.plugins.toolkit as tk
 
 from ckanext.toolbelt.decorators import Collector
 
+from . import config
+
 helper, get_helpers = Collector("flakes_rating").split()
 
 
@@ -18,3 +20,8 @@ def get_rating_list(type_: str, ids: list[str]) -> dict[str, int]:
     return tk.get_action("flakes_rating_average_list")(
         {}, {"target_ids": ids, "target_type": type_}
     )
+
+
+@helper
+def show_package_widget() -> bool:
+    return config.show_package_widget()
